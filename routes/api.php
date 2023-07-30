2460f6760fe5845dd8fd6a6f066f65a6d5008c21
Алексей Зайцев <a.zaytsev@zebrains.team>
2023-07-30 11:56:00 +0300

    feat: авторизация через sanctum [#3]

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function($route) {
+    $route->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    $route->get('/logout', function (Request $request) {
+        return $request->user()->currentAccessToken()->delete();
+    });
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken('token')->plainTextToken;
 });
