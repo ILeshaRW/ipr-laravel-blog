9d17664590eb523a5d1c3ddc28e38a2d32d28c13
Алексей Зайцев <a.zaytsev@zebrains.team>
2023-08-04 20:45:07 +0300

    feat: компонент для отображения комментариев к посту [#9]

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
+use App\View\Components\Comments;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
@@ -18,6 +20,7 @@ class CommentFactory extends Factory
         return [
             'comment' => fake()->text(),
             'post_id' => Post::inRandomOrder()->first()->id,
+            'user_id' => User::inRandomOrder()->first()->id,
         ];
     }
 }
