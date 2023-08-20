62b1dab3a972499639f8e45ad3ff1a387a6b2add
Алексей Зайцев <a.zaytsev@zebrains.team>
2023-08-20 18:56:12 +0300

    небольшой рефакторинг загрузки моделей.

--- app/Repositories/Blog/PostRepository.php
+++ app/Repositories/Blog/PostRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Blog;
 
 use App\Models\Post;
 use App\Repositories\Interfaces\IPostRepository;
+use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class PostRepository implements IPostRepository
@@ -19,20 +20,41 @@ class PostRepository implements IPostRepository
         )
             ->orderBy('created_at')
             ->active()
+            ->with(['user' => function(Builder $query) { return $query->select('id', 'name', 'last_name'); }])
+            ->withCount('comments')
             ->paginate(self::ON_PAGE);
     }
 
     /**
      * @param int $userId
+     * @param bool $isActive
      * @return LengthAwarePaginator
      */
-    public function getPostByUserId(int $userId): LengthAwarePaginator
+    public function getPostByUserId(int $userId, bool $isActive = false): LengthAwarePaginator
     {
-        return Post::addSelect(
+        $query = Post::addSelect(
             ['id', 'created_at', 'user_id', 'preview_text', 'title']
         )
             ->where('user_id', $userId)
-            ->orderBy('created_at')
-            ->paginate(self::ON_PAGE);
+            ->with('user')
+            ->withCount('comments')
+            ->orderBy('created_at');
+
+        if ($isActive) {
+            $query->active();
+        }
+
+        return $query->paginate(self::ON_PAGE);
+    }
+
+    /**
+     * Создать пост
+     *
+     * @param array $post
+     * @return Post
+     */
+    public function create(array $post): Post
+    {
+        return Post::create($post);
     }
 }
